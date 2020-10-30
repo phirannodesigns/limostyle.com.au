f5a6d2a98a0ad6a2d71f91c10e9765594ee5043a
Jd <manabat.johnder@gmail.com>
2020-10-30 13:51:06 +0800

    Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby';
 
 import { useGraphQL } from '../hooks';
 
+import { Logo } from './vectors';
+
 function Footer() {
   const {
     site: {
@@ -10,13 +12,34 @@ function Footer() {
     },
   } = useGraphQL();
   return (
-    <footer>
+    <footer className="bg-navy">
       <div className="max-w-screen-xl px-4 py-12 mx-auto overflow-hidden sm:px-6 lg:px-8">
-        <div className="mt-8">
-          <p className="text-base leading-6 text-center text-gray-400">
-            &copy; {new Date().getFullYear()} {title}. All rights reserved.
-          </p>
-        </div>
+        <Link to="/" className="block max-w-xl mx-auto">
+          <Logo className="w-full fill-current" />
+        </Link>
+
+        <ul className="mt-8 space-y-1 text-lg text-center">
+          <li className="py-1 tracking-wide">
+            <a href="tel:0423117231">0423 117 231</a>
+          </li>
+          <li className="py-1 tracking-wide">
+            <a href="mailto:info@limostyle.com.au">info@limostyle.com.au</a>
+          </li>
+          <li className="py-1 tracking-wide">
+            <p>Located in Port Macquarie</p>
+          </li>
+          <li className="py-1 tracking-wide">
+            <p>Accreditation Number: 34618</p>
+          </li>
+        </ul>
+        <a
+          href="https://www.phirannodesigns.com.au/"
+          target="_blank"
+          rel="noreferrer"
+          className="block mt-8 text-lg leading-8 tracking-wide text-center"
+        >
+          Website by Phiranno Designs
+        </a>
       </div>
     </footer>
   );
